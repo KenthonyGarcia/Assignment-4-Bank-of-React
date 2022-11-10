82c32ff6f4e71e85572462a8a23a722d5c39dd32
TzBlitz <Kenthonyg26@gmail.com>
2022-11-10 18:13:53 -0500

    Implementation of Credit-feature

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,29 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  // Create the list of Credits items
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each credits JSON array element
+      let date = credits.date.slice(0,10);
+      return <li key={credits.id}>{credits.amount} {credits.description} {date}</li>
+    }) 
+  }
   return (
     <div>
       <h1>Credits</h1>
+      <AccountBalance accountBalance={props.accountBalance}/>  
+      <br/>
+      
+      {creditsView()}
+      <form onSubmit={props.addCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">Add Credit</button>
+      </form>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
