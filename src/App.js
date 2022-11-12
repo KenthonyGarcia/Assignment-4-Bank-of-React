c4ef68950e18e29bf04f42930232c3516c8efecf
TzBlitz <Kenthonyg26@gmail.com>
2022-11-12 10:41:35 -0500

    Debugging app.js for credit page

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.credits} accountBalance={this.state.accountBalance} addCredit = {this.addCredit}/>)
+    const CreditsComponent = () => (<Credits credits={this.state.creditList}/>)
     const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
